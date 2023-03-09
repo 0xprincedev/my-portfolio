58595e92284243fba08ba2a1b19be68f12d5da12
0xprincedev <enthusiast0807@gmail.com>
2023-03-09 07:09:36 -0500

    update

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'kevingrant.0301@gmail.com';
+  $receiving_email_address = 'JamesAraki.0301@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
